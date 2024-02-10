2a49de6ae06f4796e7e03719826e1865abf33efc
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2024-02-10 21:20:02 +0530

    changes

--- app/Views/includes/header.php
+++ app/Views/includes/header.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Packachu</title>
-    <link rel="stylesheet" href="public/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="public/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/assets/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="public/assets/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/assets/css/styles.css">
     <link rel="stylesheet" href="public/fontawesome/css/all.min.css">
-    <script src="public/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="public/bootstrap/dist/js/bootstrap.js"></script>
-    <script src="public/bootstrap/dist/js/jquery-3.7.1.min.js"></script>
-    <script src="public/fontawesome/js/all.min.js"></script>
+    <script src="public/assets/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="public/assets/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="public/assets/bootstrap/dist/js/jquery-3.7.1.min.js"></script>
+    <script src="public/assets/fontawesome/js/all.min.js"></script>
 </head>
 
 <body>
