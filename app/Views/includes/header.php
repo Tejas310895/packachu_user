75a87a651dde7db6908d25df370e1801a3aa5299
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2024-02-10 22:22:16 +0530

    changes

--- app/Views/includes/header.php
+++ app/Views/includes/header.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="public/assets/bootstrap/dist/css/bootstrap.css">
     <link rel="stylesheet" href="public/assets/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/assets/css/styles.css">
-    <link rel="stylesheet" href="public/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="public/assets/fontawesome/css/all.min.css">
     <script src="public/assets/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="public/assets/bootstrap/dist/js/bootstrap.js"></script>
     <script src="public/assets/bootstrap/dist/js/jquery-3.7.1.min.js"></script>
